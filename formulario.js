c5663f84ca00df2bf44c03fa01eb58903587c9f5
rivaslucena <rivasaraujo@outlook.com>
2023-03-15 21:49:16 -0300

    error menseger

--- formulario.js
+++ formulario.js
@@ -5,13 +5,15 @@ let validador = {
 
         let inputs = form.querySelectorAll('input');
 
+        validador.clearErrors(); 
+
     //lop em cada um dos campos
         for(let i=0;i<inputs.length;i++) {
             let input = inputs[i];
             let check = validador.checkInput(input);
             if(check !== true) {
                 send = false;
-                console.log(check); //exibe o erro
+                validador.showError(input, check) 
             }
         }
 
@@ -38,6 +40,26 @@ let validador = {
             }
         }
         return true;
+    },
+    showError:(input, error) => {
+        input.style.borderColor = '#ff0000';
+
+        let errorMenseger = document.createElement('div');
+        errorMenseger.classList.add('error');
+        errorMenseger.innerHTML = error;
+
+        input.parentElement.insertBefore(errorMenseger, input.elementSibling);
+    }, 
+    clearErrors:() => {
+        let inputs = form.querySelectorAll('input');
+        for(let i=0;i<inputs.length;i++) {
+            inputs[i].style = '';
+        }
+
+        let errorMenseger = document.querySelectorAll('.error');
+        for(let i=0;i<errorMenseger.length;i++) {
+            errorMenseger[i].remove();
+        }
     }
 };
 
