fa1ffd4314573e66bbe97226b4a2ef146804b3e0
rivaslucena <rivasaraujo@outlook.com>
2023-03-19 10:13:24 -0300

    criacao de portfolio

---
+++ README.md
@@ -0,0 +1,3 @@
+# validador de cadastro
+![validador-de-cadastro](./images/img1.jpg)
+Validador de cadastro, que pega as informações fornecida pelo usúario em seus campos obrigatorios. feito pelo Rivelino Araújo.
--- formulario.js
+++ formulario.js
@@ -41,11 +41,11 @@ let validador = {
                     case 'email':
                         if(input.value != '') {
                             let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-                            if(regex.test(input.value.toLowerCase())) {
-                                return 'Digite um email válido';
+                            if(!regex.test(input.value.toLowerCase())) {
+                                return 'E-mail digitado não é válido!';
                             }
                         }
-                        break;
+                    break;
                 }
             }
         }
